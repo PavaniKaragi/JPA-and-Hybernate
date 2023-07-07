35a16038449c36e91de305dc59c10c15211d47a9
PavaniKaragi <135801376+PavaniKaragi@users.noreply.github.com>
2023-07-07 17:40:38 +0530

    Add files via upload

---
+++ learn-jpa-hybernate/learn-jpa-hybernate/src/main/java/com/springboot/learnjpahybernate/LearnJpaHybernateApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.learnjpahybernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaHybernateApplication { // launches the spring context
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaHybernateApplication.class, args);
+	}
+
+}
---
+++ learn-jpa-hybernate/learn-jpa-hybernate/target/classes/data.sql
@@ -0,0 +1,3 @@
+create table course(id int ,name varchar(255) not null,author varchar(255) not null)
+
+
